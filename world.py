fcfbba3ca1e74ee398434f9e97b70c5fa0029813
Gostark <muhsinpootheri@gmail.com>
2025-09-17 23:00:24 +0530

    player controls added

--- world.py
+++ world.py
@@ -1,16 +1,30 @@
+def move_player(move, p_r, p_c):
+    if move == 'W' and p_r > 0:
+        p_r -= 1
+    elif move == 'S' and p_r < 9:  # Changed to 9 for 10x5 grid
+        p_r += 1
+    elif move == 'A' and p_c > 0:
+        p_c -= 1
+    elif move == 'D' and p_c < 4:  # Keep at 4 for 5 columns
+        p_c += 1
+    
+    return p_r, p_c
 
-choice = 'y'
-while choice != 'n':
-    print("Enter the position of player :- ")
-    p_r = input("row: ")
-    p_c = input("column: ")
-    for i in range (10):
-        for j in range (5):
-            if i == int(p_r) and j == int(p_c):
-                print("@", end=" ")  # Print '@' for player position
+def print_grid(p_r, p_c):
+    for i in range(10):
+        for j in range(5):
+            if i == p_r and j == p_c:
+                print("@", end=" ")
             else:
-                print("*", end=" ")    # Add space between asterisks
-        print()
-    choice = input("Do you want to change the position of player? (y/n): ") #menu to change player position
+                print("*", end=" ")
+        print()  # New line after each row
 
-                    # Add new line after each row
+# Initialize player position
+p_r, p_c = 2, 2  # Starting position
+choice = 'y'
+
+while choice != 'n':
+    print_grid(p_r, p_c)  # Display current grid
+    move = input("Move (W,A,S,D): ").upper()
+    p_r, p_c = move_player(move, p_r, p_c)
+    choice = input("Continue? (y/n): ")
